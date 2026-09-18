d50b8b26c2cb26849aa43c191ed73a2f0f7bf405
Akshayyaaa <akshayadeviss08@gmail.com>
2026-09-18 11:34:42 +0530

    authenticate successfull

--- core/settings.py
+++ core/settings.py
@@ -65,13 +65,15 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ALLOW_ALL_ORIGINS = True
+
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES' :
-    ('rest_framework_simplejwt.authentication.JWTAuthentication',),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny'
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
     ),
-}
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated'
+        ),
+}   
 
 ROOT_URLCONF = 'core.urls'
 
